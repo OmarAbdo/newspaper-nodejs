3a17e867518c6ef040594522d28d6f891a65612e
Omar Abdo <omareabdo@gmail.com>
2019-07-05 20:19:06 +0200

    login function implemented

--- controllers/auth/authentication.js
+++ controllers/auth/authentication.js
@@ -1,26 +1,74 @@
-import User from '../../models/user';
 import { check, validationResult } from 'express-validator/check';
-import allCountries from '../../util/countries';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+import User from '../../models/user';
+import allCountries from '../../util/countries';
+import config from '../../util/config';
+
+/**
+ * Authentication Class
+ * 
+ * @description this class is responsible for user authentication which includes the following method  
+ * @method signUp               - for user registration 
+ * @method logIn                - for user login 
+ * @method validate             - validates both login and registration forms' data
+ * @method passwordResetToken   - sending email with a reset token to a user to reset his password
+ * @method passwordResetForm    - accepting the new user password if he followed the link sent to his email address 
+ * @method checkToken           - to validate that users are authentic and can be allowed to visit the desired routes 
+ * 
+ * @author Omar Abdo
+ */
 class AuthenticationController {
+
     signUp(req, res) {
         console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
-        //create jwt here
-      
-        bcrypt.hash(req.body.password, 10, function(err, hash) {
-            User.build({ ...req.body, password: hash})            
-            .save()
-                .then(result => res.status(200).json({success: 'User Created Successfully',}))
+        bcrypt.hash(req.body.password, 10, function (err, hash) {
+            User.build({ ...req.body, password: hash })
+                .save()
+                .then(result => {
+                    res.status(200).json({
+                        token: jwt.sign({ userId: result.id }, config.privateKey, { expiresIn: 60 * 60 }),
+                        success: 'User Created Successfully',
+                        authenticated: true,
+                    })
+                })
                 .catch(error => res.status(422).json({ error: error }))
         });
-       
     }
 
+    logIn(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        User.findOne({ where: { email: req.body.email } }).then(user => {
+            if (user) {
+                bcrypt.compare(req.body.password, user.password, function (err, match) {
+                    if (match) {
+                        res.status(200).json({
+                            token: jwt.sign({ userId: res.id }, config.privateKey, { expiresIn: 60 * 60 }),
+                            success: 'User Logged In Successfully',
+                            authenticated: true,
+                        });
+                    } else {
+                        res.status(422).json({
+                            error: 'Email / password is wrong',
+                            authenticated: false,
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * @todo try to avoid duplicating the validation code used for the signUp with the login validation
+     */
     validate(method) {
         switch (method) {
             case 'signUp': {
@@ -48,7 +96,6 @@ class AuthenticationController {
                             if (value == req.body.email) {
                                 throw new Error('Passwords can\'t be the same as email ');
                             } else {
-                                //hash password here
                                 return value;
                             }
                         })
@@ -74,7 +121,56 @@ class AuthenticationController {
                     })
                 ]
             }
+            case 'logIn': {
+                return [
+                    check('email').isEmail(),
+                    check('password', 'password must be 8+ characters contain at least a number, a capital letter and a special character')
+                        .isLength({ min: 8 })
+                        .custom((value, { req, loc, path }) => {
+                            if (value == req.body.email) {
+                                throw new Error('Passwords can\'t be the same as email ');
+                            } else {
+                                return value;
+                            }
+                        })
+                        .matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/),
+                ];
+            }
         }
+    }
+
+    checkToken(req, res, next) {
+        const header = req.headers['authorization'];
+
+        if (typeof header !== 'undefined') {
+            req.token = header;
+            jwt.verify(req.token, config.privateKey, (err) => {
+                if (err) {
+                    res.status(403).json({
+                        error: "Please Login again 1",
+                    })
+                } else {
+                    next();
+                }
+            })
+
+        } else {
+            res.status(403).json({ error: "Please Login again 2" })
+        }
+    }
+
+    /**
+     * @todo delete this late
+     */
+    dummyUser(req, res) {
+        console.log(req.body);
+        res.status(200).json({
+            name: 'John Locke',
+            email: 'johnlock@liberalism.com',
+            password: '123flutteriscool.com',
+            country: 'England',
+            birthday: '29/08/1632',
+        })
     }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,24 @@ import authenticationController from '../controllers/auth/authentication';
 
 const router = express.Router();
 
-router.get('/api/v1/todos', userController.getAllTodos);
-router.get('/api/v1/todos/:id', userController.getTodo);
-router.post('/api/v1/todos', userController.createTodo);
-router.put('/api/v1/todos/:id', userController.updateTodo);
-router.delete('/api/v1/todos/:id', userController.deleteTodo);
+//protected authenticated-users only routes
+router.get('/api/v1/todos', authenticationController.checkToken, userController.getAllTodos);
+router.get('/api/v1/todos/:id', authenticationController.checkToken, userController.getTodo);
+router.post('/api/v1/todos', authenticationController.checkToken, userController.createTodo);
+router.put('/api/v1/todos/:id', authenticationController.checkToken, userController.updateTodo);
+router.delete('/api/v1/todos/:id', authenticationController.checkToken, userController.deleteTodo);
 
+
+//authentication routes
+router.post('/api/v1/authentication/signup', authenticationController.validate('signUp'), authenticationController.signUp);
+// router.post('/api/v1/authentication/login',  authenticationController.logIn);
+router.post('/api/v1/authentication/login', authenticationController.validate('logIn'), authenticationController.logIn);
+// forgot password route
+
+//Testing routes
 router.get('/api/v1/test/authenticate', experimentalController.createJWT);
-
-
-router.post('/api/v1/authentication/signUp', authenticationController.validate('signUp'), authenticationController.signUp);
+router.get('/api/v1/authentication/getdummyuser', authenticationController.dummyUser);
+//router.get('/api/v1/authentication/login',  authenticationController.logIn);
 
 
 export default router;
